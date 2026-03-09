50164fc58a8f734aa11dd98ddc6df9e71c9ab24f
RETHINK.FFILHO <rethink.ffilho@voegol.com.br>
2026-03-09 19:51:18 -0300

    feat: added unit tests and config testing liberty

---
+++ src/test-utils/renderApp.tsx
@@ -0,0 +1,80 @@
+import HomeScreen from "@/app/(protected)/(tabs)";
+import TabLayout from "@/app/(protected)/(tabs)/_layout";
+import ExploreScreen from "@/app/(protected)/(tabs)/explore";
+import ProfileScreen from "@/app/(protected)/(tabs)/profile";
+import ProtectedLayout from "@/app/(protected)/_layout";
+import CityDetails from "@/app/(protected)/city-details/[id]";
+import SignInScreen from "@/app/sign-in";
+import SignUpScreen from "@/app/sign-up";
+import { renderRouter } from "expo-router/testing-library";
+import { AppStack } from "../ui/navigation/AppStack";
+
+import { Toast } from "@/src/infra/feedbackService/adapterts/Toast/Toast";
+import { ToastFeedback } from "@/src/infra/feedbackService/adapterts/Toast/ToastFeedback";
+import { FeedbackProvider } from "@/src/infra/feedbackService/IFeedbackProvider";
+import { InMemoryRepository } from "@/src/infra/repositories/adapterts/inMemory";
+import { ThemeProvider } from "@shopify/restyle";
+import { AuthProvider } from "../domain/auth/AuthContext";
+import { RepositoryProvider } from "../infra/repositories/RepositoryProvider";
+import { inMemoryStorage } from "../infra/storage/adapterts/InMemoryStorage";
+import { StorageProvider } from "../infra/storage/StorageContext";
+import theme from "../ui/theme/theme";
+
+function MockedAuthProvider({ children }: React.PropsWithChildren) {
+  const authUser: AuthUser = {
+    email: "lucas@coffstack.com",
+    id: "1",
+    fullname: "Lucas Garcez",
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isReady: true,
+        authUser,
+        saveAuthUser: async () => {},
+        removeAuthUser: async () => {},
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function renderApp(options?: { isAuthenticated?: boolean }) {
+  const FinalAuthProvider = options?.isAuthenticated
+    ? MockedAuthProvider
+    : AuthProvider;
+
+  function Wrapper({ children }: React.PropsWithChildren) {
+    return (
+      <StorageProvider storage={inMemoryStorage}>
+        <FinalAuthProvider>
+          <FeedbackProvider value={ToastFeedback}>
+            <RepositoryProvider value={InMemoryRepository}>
+              <ThemeProvider theme={theme}>
+                {children}
+                <Toast />
+              </ThemeProvider>
+            </RepositoryProvider>
+          </FeedbackProvider>
+        </FinalAuthProvider>
+      </StorageProvider>
+    );
+  }
+
+  renderRouter(
+    {
+      _layout: () => <AppStack />,
+      "(protected)/_layout": () => <ProtectedLayout />,
+      "(protected)/(tabs)/_layout": () => <TabLayout />,
+      "(protected)/(tabs)/index": () => <HomeScreen />,
+      "(protected)/(tabs)/explore": () => <ExploreScreen />,
+      "(protected)/(tabs)/profile": () => <ProfileScreen />,
+      "(protected)/city-details/[id]": () => <CityDetails />,
+      "sign-in": () => <SignInScreen />,
+      "sign-up": () => <SignUpScreen />,
+    },
+    { wrapper: Wrapper, initialUrl: "/" }
+  );
+}
